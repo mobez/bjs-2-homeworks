1e2ce2e8b0eb0a6b7a3e79310aa5da250aba2b2d
mobez <aanik@bk.ru>
2022-06-26 17:36:14 +1000

    modifi task3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length)
+    result = arr1.every((item, indx) => item === arr2[indx]);
+  else
+    result = false;
+  console.log("Задание 1:", result);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter((item) => item > 0 && !(item % 3)).map((item) => item * 10);
+  console.log("Задание 2:", resultArr);
   return resultArr; // array
 }
